e708edeb063f67e253de7124b61da59c1bfa2c18
Fabian <fabianejp1@gmail.com>
2023-05-20 17:52:59 -0400

    Create Post, Edit & Delete creted

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
-    path( "", BlogListView.as_view(), name="home" ),
-    path( "post/<int:pk>", BlogDetailView.as_view(), name="post_detail" ),
+    path( "", BlogListView.as_view(), name="home" ),                            # Home
+    path( "post/<int:pk>", BlogDetailView.as_view(), name="post_detail" ),      # Post detail
+    path( "post/new/", BlogCreateView.as_view(), name="post_new"),              # Create a new post
+    path( "post/<int:pk>/edit", BlogUpdateView.as_view(), name="post_edit"),    # Edit a post
+    path( "post/<int:pk>/delete", BlogDeleteView.as_view(), name="post_delete"),# Delete a post
 ]
